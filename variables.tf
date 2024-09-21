1e05ac15136bbe3d026ba1a5640db658b3f5ad93
sharvani28 <sharvanikandala@gmail.com>
2024-09-21 13:18:11 +1000

    part4 files

---
+++ variables.tf
@@ -0,0 +1,13 @@
+# Sets global variables for this Terraform project.
+
+variable app_name {
+  default = "sharvani1app123"
+}
+
+variable location {
+  default = "australiasoutheast"
+}
+
+variable kubernetes_version {    
+  default = "1.30.3"
+}
